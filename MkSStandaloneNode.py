fad8c3a21f339f05f4590d07326cd0bc3c0629e0
Yevgeniy Kiveisha <yevgeniy.kiveisha.com>
2020-05-18 15:25:53 +0300

    start of standalone node
    Signed-off-by: Yevgeniy Kiveisha <yevgeniy.kiveisha.com>

---
+++ MkSStandaloneNode.py
@@ -0,0 +1,249 @@
+#!/usr/bin/python
+import os
+import sys
+import json
+import time
+if sys.version_info[0] < 3:
+	import thread
+else:
+	import _thread
+import threading
+import socket
+import subprocess
+from subprocess import call
+import urllib2
+import urllib
+
+from flask import Flask, render_template, jsonify, Response, request
+import logging
+
+import MkSGlobals
+from mksdk import MkSFile
+from mksdk import MkSNetMachine
+from mksdk import MkSAbstractNode
+from mksdk import MkSShellExecutor
+
+class StandaloneNode(MkSAbstractNode.AbstractNode):
+	def __init__(self):
+		MkSAbstractNode.AbstractNode.__init__(self)
+		self.ClassName 							= "Standalone Node"
+		# Members
+		self.HostName							= socket.gethostname()
+		self.IsMaster 							= False
+		# Node connection to WS information
+		self.GatewayIP 							= ""
+		self.ApiPort 							= "8080"
+		self.WsPort 							= "1981"
+		self.ApiUrl 							= ""
+		self.WsUrl								= ""
+		self.UserName 							= ""
+		self.Password 							= ""
+		# Locks and Events
+		self.NetworkAccessTickLock 				= threading.Lock()
+		# Sates
+		self.States 							= {
+			'IDLE': 							self.StateIdle,
+			'INIT':								self.StateInit,
+			'INIT_GATEWAY':						self.StateInitGateway,
+			'ACCESS_GATEWAY': 					self.StateAccessGetway,
+			'ACCESS_WAIT_GATEWAY':				self.StateAccessWaitGatway,
+			'INIT_LOCAL_SERVER':				self.StateInitLocalServer,
+			'WORKING': 							self.StateWork
+		}
+		# Handlers
+		self.NodeRequestHandlers['get_port'] 		= self.GetPortRequestHandler
+		self.NodeRequestHandlers['get_local_nodes'] = self.GetLocalNodesRequestHandler
+		self.NodeRequestHandlers['get_master_info'] = self.GetMasterInfoRequestHandler
+		self.NodeRequestHandlers['upload_file'] 	= self.UploadFileHandler
+		# Callbacks
+		self.GatewayDataArrivedCallback 		= None
+		self.GatewayConnectedCallback 			= None
+		self.GatewayConnectionClosedCallback 	= None
+		self.OnCustomCommandRequestCallback		= None
+		self.OnCustomCommandResponseCallback	= None
+		# Flags
+		self.IsListenerEnabled 					= False
+		self.PipeStdoutRun						= False
+		self.SetState("INIT")
+
+	#
+	# ###### STANDALONE NODE INITIATE ->
+	#
+
+	def Initiate(self):
+		self.LoadNodesOnMasterStart()
+
+	#
+	# ###### STANDALONE NODE INITIATE <-
+	#
+
+	#
+	# ###### STANDALONE NODE STATES ->
+	#
+
+	def StateIdle (self):
+		print ("({classname})# Note, in IDLE state ...".format(classname=self.ClassName))
+		time.sleep(1)
+	
+	def StateInit (self):
+		self.SetState("INIT_LOCAL_SERVER")
+	
+	def StateInitGateway(self):
+		if self.IsNodeWSServiceEnabled is True:
+			# Create Network instance
+			self.Network = MkSNetMachine.Network(self.ApiUrl, self.WsUrl)
+			self.Network.SetDeviceType(self.Type)
+			self.Network.SetDeviceUUID(self.UUID)
+			# Register to events
+			self.Network.OnConnectionCallback  		= self.WebSocketConnectedCallback
+			self.Network.OnDataArrivedCallback 		= self.WebSocketDataArrivedCallback
+			self.Network.OnConnectionClosedCallback = self.WebSocketConnectionClosedCallback
+			self.Network.OnErrorCallback 			= self.WebSocketErrorCallback
+			self.AccessTick = 0
+
+			self.SetState("ACCESS_GATEWAY")
+		else:
+			if self.IsNodeLocalServerEnabled is True:
+				self.SetState("INIT_LOCAL_SERVER")
+			else:
+				self.SetState("WORKING")
+
+	def StateAccessGetway (self):
+		if self.IsNodeWSServiceEnabled is True:
+			self.Network.AccessGateway(self.Key, json.dumps({
+				'node_name': str(self.Name),
+				'node_type': self.Type
+			}))
+
+			self.SetState("ACCESS_WAIT_GATEWAY")
+		else:
+			self.SetState("WORKING")
+	
+	def StateAccessWaitGatway (self):
+		if self.AccessTick > 10:
+			self.SetState("ACCESS_WAIT_GATEWAY")
+			self.AccessTick = 0
+		else:
+			self.AccessTick += 1
+	
+	def StateInitLocalServer(self):
+		self.ServerAdderss = ('', 16999)
+		status = self.TryStartListener()
+		if status is True:
+			self.IsListenerEnabled = True
+			self.SetState("INIT_GATEWAY")
+		time.sleep(1)
+
+	def StateWork (self):
+		if self.SystemLoaded is False:
+			self.SystemLoaded = True # Update node that system done loading.
+			if self.NodeSystemLoadedCallback is not None:
+				self.NodeSystemLoadedCallback()
+	
+	#
+	# ###### STANDALONE NODE STATES <-
+	#
+	
+	#
+	# ###### STANDALONE NODE GATAWAY CALLBACKS ->
+	#
+
+	def WebSocketConnectedCallback (self):
+		self.SetState("WORKING")
+		if self.GatewayConnectedCallback is not None:
+			self.GatewayConnectedCallback()
+	
+	def WebSocketConnectionClosedCallback (self):
+		if self.GatewayConnectionClosedCallback is not None:
+			self.GatewayConnectionClosedCallback()
+		self.NetworkAccessTickLock.acquire()
+		try:
+			self.AccessTick = 0
+		finally:
+			self.NetworkAccessTickLock.release()
+		self.SetState("ACCESS_GATEWAY")
+
+	# TODO - Must be dealt with different thread (we must have thread poll)
+	def WebSocketDataArrivedCallback (self, packet):
+		self.NetworkAccessTickLock.acquire()
+		try:
+			self.SetState("WORKING")
+			messageType = self.BasicProtocol.GetMessageTypeFromJson(packet)
+			direction 	= self.BasicProtocol.GetDirectionFromJson(packet)
+			destination = self.BasicProtocol.GetDestinationFromJson(packet)
+			source 		= self.BasicProtocol.GetSourceFromJson(packet)
+			command 	= self.BasicProtocol.GetCommandFromJson(packet)
+
+			packet["additional"]["client_type"] = "global_ws"
+
+			print ("({classname})# [{direction}] {source} -> {dest} [{cmd}]".format(
+						classname=self.ClassName,
+						direction=direction,
+						source=source,
+						dest=destination,
+						cmd=command))
+			
+			if messageType == "BROADCAST":
+				pass
+		
+			if destination in source:
+				self.NetworkAccessTickLock.release()
+				return
+
+			# Is this packet for me?
+			if destination in self.UUID:
+				if messageType == "CUSTOM":
+					self.NetworkAccessTickLock.release()
+					return
+				elif messageType in ["DIRECT", "PRIVATE", "WEBFACE"]:
+					if command in self.NodeRequestHandlers.keys():
+						message = self.NodeRequestHandlers[command](None, packet)
+						self.Network.SendWebSocket(message)
+					else:
+						if self.GatewayDataArrivedCallback is not None:
+							self.GatewayDataArrivedCallback(None, packet)
+				else:
+					print ("({classname})# [Websocket INBOUND] ERROR - Not support {0} request type.".format(messageType, classname=self.ClassName))
+			else:
+				print ("({classname})# Not mine ... Sending to slave ...".format(classname=self.ClassName))
+		except Exception as e:
+			print("({classname})# WebSocket Error - Data arrived issue\nPACKET#\n{0}\n(EXEPTION)# {error}".format(
+						packet,
+						classname=self.ClassName,
+						error=str(e)))
+		self.NetworkAccessTickLock.release()
+	
+	def WebSocketErrorCallback (self):
+		print ("({classname})# ERROR - Gateway socket error".format(classname=self.ClassName))
+		# TODO - Send callback "OnWSError"
+		self.NetworkAccessTickLock.acquire()
+		try:
+			self.AccessTick = 0
+		finally:
+			self.NetworkAccessTickLock.release()
+		self.SetState("ACCESS_WAIT_GATEWAY")
+	
+	#
+	# ###### STANDALONE NODE GATAWAY CALLBACKS <-
+	#
+
+	def GetNodeInfoRequestHandler(self, sock, packet):
+		payload = self.NodeInfo
+		payload["is_master"] 	= True
+		payload["master_uuid"] 	= ""
+		return self.Network.BasicProtocol.BuildResponse(packet, payload)
+
+	#
+	# ##################################################################################################
+	#
+
+	def SendRequest(self, uuid, msg_type, command, payload, additional):
+		# Generate request
+		message = self.Network.BasicProtocol.BuildRequest(msg_type, uuid, self.UUID, command, payload, additional)
+		# TODO - Check if we need to send it via Gateway
+		# Send message
+		self.Network.SendWebSocket(message)
+	
+	#
+	# ##################################################################################################
+	#
