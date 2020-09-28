130a52bc96bc7e3525e43c4bf515c55df0088816
Yevgeniy Kiveisha <yevgeniy.kiveisha@gmail.com>
2020-09-28 10:34:46 +0300

    commit #12
    Signed-off-by: Yevgeniy Kiveisha <yevgeniy.kiveisha@gmail.com>

--- MkSExternalMasterList.py
+++ MkSExternalMasterList.py
@@ -24,6 +24,7 @@ class ExternalMasterList:
 		self.Context.NodeResponseHandlers['get_master_nodes'] = self.OnGetMasterNodesResponseHandler
 
 	def OnGetMasterNodesRequestHandler(self, sock, packet):
+		self.Context.LogMSG("({classname})# [OnGetMasterNodesRequestHandler]".format(classname=self.ClassName),5)
 		connections = self.Context.GetConnectedNodes()
 
 		nodes_list = []
@@ -37,6 +38,7 @@ class ExternalMasterList:
 		})
 
 	def OnGetMasterNodesResponseHandler(self, sock, packet):
+		self.Context.LogMSG("({classname})# [OnGetMasterNodesResponseHandler]".format(classname=self.ClassName),5)
 		payload = self.Context.BasicProtocol.GetPayloadFromJson(packet)
 		self.Masters[payload["ip"]]["nodes"] = payload["nodes"]
 		self.Masters[payload["ip"]]["ts"] 	 = time.time()
@@ -54,11 +56,12 @@ class ExternalMasterList:
 
 	def SendGetNodesList(self, master):
 		if (time.time() - master["ts"] > 60):
-			if master["conn"] is not None:
-				message = self.Conext.BasicProtocol.BuildRequest("DIRECT", "MASTER", self.UUID, "get_master_nodes", {}, {})
-				packet  = self.Conext.BasicProtocol.AppendMagic(message)
-				self.Conext.SocketServer.Send(master["conn"].Socket, packet)
 			master["ts"] = time.time()
+			if master["conn"] is not None:
+				message = self.Context.BasicProtocol.BuildRequest("DIRECT", "MASTER", self.Context.UUID, "get_master_nodes", {}, {})
+				packet  = self.Context.BasicProtocol.AppendMagic(message)
+				self.Context.SocketServer.Send(master["conn"].Socket, packet)
+			self.Context.LogMSG("({classname})# SendGetNodesList [{0}, {1}] <get_master_nodes>".format(master["ip"],master["ts"],classname=self.ClassName),5)
 
 	def Worker(self):
 		self.Context.LogMSG("({classname})# Start worker".format(classname=self.ClassName),5)
@@ -67,11 +70,9 @@ class ExternalMasterList:
 			try:
 				for key in self.Masters:
 					master = self.Masters[key]
-					# self.Context.LogMSG("({classname})# Worker {0}".format(master,classname=self.ClassName),5)
 					if master["status"] is False:
 						# Connect to the master
 						# Check if this socket already exist !!!!!!!
-						self.Context.LogMSG("({classname})# ConnectMasterWithRetries [{0}]".format(master["ip"],classname=self.ClassName),5)
 						conn, status = self.ConnectMasterWithRetries(master["ip"])
 						if status is True:
 							master["conn"] 	 = conn
@@ -85,7 +86,7 @@ class ExternalMasterList:
 						self.SendGetNodesList(master)
 					time.sleep(1)
 			except Exception as e:
-				self.Context.LogException("[Worker] {0}".format(command),e,3)
+				self.Context.LogException("[Worker]",e,3)
 
 	def Append(self, master):
 		if master["ip"] in self.Masters or master["ip"] in self.Context.MyLocalIP:
